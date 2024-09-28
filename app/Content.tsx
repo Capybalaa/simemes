86d6109d48a3bef996067b1356f2aeee9fb9881a
capybalaa <capybalaa@gmail.com>
2024-09-28 10:57:17 +0800

    update

--- app/Content.tsx
+++ app/Content.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Content() {
   return (
     <div
-      className="min-h-dvh mx-auto xl:max-w-xl bg-no-repeat bg-bottom bg-contain p-10 flex flex-col gap-10"
+      className="min-h-dvh mx-auto md:max-w-xl bg-no-repeat bg-bottom bg-contain p-10 flex flex-col gap-10"
       style={{ backgroundImage: "url(/all2.png)" }}
     >
       <Image
