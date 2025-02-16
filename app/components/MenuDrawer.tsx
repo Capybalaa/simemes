dc10b14d5f4af27ea807b10673bc39aa469d6b85
yujuiting <yujuiting@me.com>
2025-02-16 19:00:32 +0800

    fix menu

--- app/components/MenuDrawer.tsx
+++ app/components/MenuDrawer.tsx
@@ -3,9 +3,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
+import { createPortal } from 'react-dom'
 
 export default function MenuDrawer() {
   const [isOpen, setIsOpen] = useState(false)
+
   return (
     <>
       <button onClick={() => setIsOpen(true)}>
@@ -17,66 +19,72 @@ export default function MenuDrawer() {
           height={80}
         />
       </button>
-      <div
-        className={[
-          'max-w-md w-full h-dvh fixed top-0 right-0 transition-transform bg-[#FAFDFB]',
-          isOpen ? 'translate-x-0' : 'translate-x-full',
-          'flex flex-col z-50',
-        ].join(' ')}
-      >
-        <div className="bg-[#F2EFEA] h-14 px-4 flex flex-row items-center justify-end">
-          <button className="block xl:hidden" onClick={() => setIsOpen(false)}>
-            <Image
-              className="w-10 h-10"
-              src="/close.png"
-              alt="close"
-              width={80}
-              height={80}
-            />
-          </button>
-        </div>
-        <div className="grow flex flex-col gap-10 justify-center items-center capitalize text-[32px] font-bebas-neue py-10">
-          <Image
-            className="w-[163px] h-[60px]"
-            src="/logo.png"
-            alt="simemes"
-            width={326}
-            height={120}
-          />
-          <Link href="#">story</Link>
-          <Link href="#">about</Link>
-          <div className="flex flex-row gap-10">
-            <Link href="#">
+      {createPortal(
+        <div
+          className={[
+            'max-w-md w-full h-dvh fixed top-0 right-0 transition-transform bg-[#FAFDFB]',
+            isOpen ? 'translate-x-0' : 'translate-x-full',
+            'flex flex-col z-50',
+          ].join(' ')}
+        >
+          <div className="bg-[#F2EFEA] h-14 px-4 flex flex-row items-center justify-end">
+            <button
+              className="block xl:hidden"
+              onClick={() => setIsOpen(false)}
+            >
               <Image
-                className="w-12 h-12"
-                src="/social-x.png"
-                alt="x"
-                width={96}
-                height={96}
+                className="w-10 h-10"
+                src="/close.png"
+                alt="close"
+                width={80}
+                height={80}
               />
-            </Link>{' '}
-            <Link href="#">
-              <Image
-                className="w-12 h-12"
-                src="/social-tg.png"
-                alt="x"
-                width={96}
-                height={96}
-              />
-            </Link>
+            </button>
           </div>
+          <div className="grow flex flex-col gap-10 justify-center items-center capitalize text-[32px] font-bebas-neue py-10">
+            <Image
+              className="w-[163px] h-[60px]"
+              src="/logo.png"
+              alt="simemes"
+              width={326}
+              height={120}
+            />
+            <Link href="#">story</Link>
+            <Link href="#">about</Link>
+            <div className="flex flex-row gap-10">
+              <Link href="#">
+                <Image
+                  className="w-12 h-12"
+                  src="/social-x.png"
+                  alt="x"
+                  width={96}
+                  height={96}
+                />
+              </Link>{' '}
+              <Link href="#">
+                <Image
+                  className="w-12 h-12"
+                  src="/social-tg.png"
+                  alt="x"
+                  width={96}
+                  height={96}
+                />
+              </Link>
+            </div>
 
-          <div className="grow" />
+            <div className="grow" />
 
-          <Image
-            className="w-[245px] h-[158px]"
-            src="/candy.png"
-            width={982}
-            height={632}
-            alt="candy"
-          />
-        </div>
-      </div>
+            <Image
+              className="w-[245px] h-[158px]"
+              src="/candy.png"
+              width={982}
+              height={632}
+              alt="candy"
+            />
+          </div>
+        </div>,
+        document.body
+      )}
     </>
   )
 }
